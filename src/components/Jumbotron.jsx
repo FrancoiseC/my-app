8df17b0a85f103689e867baf12772983c6fafc84
Francoise <84253938+FrancoiseC@users.noreply.github.com>
2024-03-02 01:58:35 +0000

    TimeBomb

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap";
+export default function Jumbotron (){
+    return(
+        <>
+        <Container>
+            <Row>
+                <Col>
+                <h1>
+                    okkkkkkk
+                </h1>
+                </Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+
+import { CounterContainer } from "@/components/CounterContainer";
+import { Container } from "react-bootstrap";
+
+export default function Home () {
+  return(
+    <Container>
+      
+      <CounterContainer/>
+    </Container>
+  );
+}
